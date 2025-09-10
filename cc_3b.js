45ded4c44237bceecf93bae4bd578af995cbb58c
Joshua Glaser <jdglaser@usf.edu>
2025-09-10 17:28:37 -0400

    updated to include step 2

---
+++ cc_3b.js
@@ -0,0 +1,33 @@
+//Step 2
+let inventory = [
+{
+    sku: "SKU-1001",
+    name: "AirPods",
+    price: "199.00",
+    stock: "210"
+
+},
+{
+    sku: "SKU- 1002",
+    name: "iPod",
+    price: "299.00",
+    stock: "15"
+
+},
+{
+    sku: "SKU-1003",
+    name: "iPhone",
+    price: "649.00",
+    stock: "205"
+
+},
+{
+    sku: "SKU-1004",
+    name: "iPad",
+    price: "499.00",
+    stock: "115"
+
+}
+
+]
+console.log(inventory)
